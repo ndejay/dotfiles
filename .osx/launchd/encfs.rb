30c59dd91ed05dbd07814b74bf1c3dc7525ca349
Nicolas De Jay <ndj@pinkfilter.org>
2015-06-18 16:01:28 -0400

    Use YAML in place of Ruby for launchd config files

--- .osx/launchd/encfs.rb
+++ .osx/launchd/encfs.rb
@@ -1,18 +1,20 @@
 #!/usr/bin/ruby
 
-require_relative 'encfs.local'
+require 'yaml'
 
+path           = File.dirname(__FILE__)
+config         = YAML.load_file("#{path}/encfs.yml")
 active_mounts  = `mount`
 
-$mounts.each do |mount|
-  source      = File.expand_path(mount[:source])
-  destination = File.expand_path(mount[:destination])
-  volname     = mount[:volname] || File.basename(destination)
-  options     = ($options + ["volname='#{volname}'"]).join(',')
+config['mounts'].each do |mount|
+  source      = File.expand_path(mount['source'])
+  destination = File.expand_path(mount['destination'])
+  volname     = mount['volname'] || File.basename(destination)
+  options     = (config['options'] + ["volname='#{volname}'"]).join(',')
   is_mounted  = active_mounts.include? destination
 
   if !is_mounted
-    `echo '#{mount[:password]}' | #{$encfs_path} \
+    `echo '#{mount['password']}' | #{config['encfs_path']} \
       -o #{options} -S '#{source}' '#{destination}'`
   end
 end
--- .osx/launchd/sshfs.rb
+++ .osx/launchd/sshfs.rb
@@ -1,19 +1,21 @@
 #!/usr/bin/ruby
 
-require_relative 'sshfs.local'
+require 'yaml'
 
+path           = File.dirname(__FILE__)
+config         = YAML.load_file("#{path}/sshfs.yml")
 active_mounts  = `mount`
 
-$mounts.each do |mount|
-  destination  = File.expand_path(mount[:destination])
-  volname      = mount[:volname] || File.basename(destination)
-  options      = ($options + ["volname='#{volname}'"]).join(',')
-  port         = mount[:port] ? "-p #{mount[:port]}" : ''
+config['mounts'].each do |mount|
+  destination  = File.expand_path(mount['destination'])
+  volname      = mount['volname'] || File.basename(destination)
+  options      = (config['options'] + ["volname='#{volname}'"]).join(',')
+  port         = mount['port'] ? "-p #{mount['port']}" : ''
   is_mounted   = active_mounts.include? destination
 
   if !is_mounted 
     system 'mkdir', '-p', destination
-    `#{$sshfs_path} #{port} -o #{options} \
-      '#{mount[:source]}' '#{destination}'`
+    `#{config['sshfs_path']} #{port} -o #{options} \
+      '#{mount['source']}' '#{destination}'`
   end
 end
