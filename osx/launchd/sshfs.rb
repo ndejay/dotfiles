380ea1cf528a6f9ebcd5d360bb3748825c37afd4
Nicolas De Jay <ndj@pinkfilter.org>
2016-10-21 14:44:03 -0400

    Fix active mount parsing.

--- osx/launchd/sshfs.rb
+++ osx/launchd/sshfs.rb
@@ -4,18 +4,22 @@ require 'yaml'
 
 path           = File.dirname(__FILE__)
 config         = YAML.load_file("#{path}/sshfs.yml")
-active_mounts  = `mount`
+active_mounts  = `mount`.split("\n").map do |line|
+  l = line.split(" on ")
+  [l[1].split(" (")[0], l[0]]
+end
+active_mounts  = Hash[active_mounts]
 
 config['mounts'].each do |mount|
   destination  = File.expand_path(mount['destination'])
   volname      = mount['volname'] || File.basename(destination)
   options      = (config['options'] + ["volname='#{volname}'"]).join(',')
   port         = mount['port'] ? "-p #{mount['port']}" : ''
-  is_mounted   = active_mounts.include? destination
+  is_mounted   = active_mounts.keys.include? destination
 
-  if !is_mounted 
+  if !is_mounted
     system 'mkdir', '-p', destination
-    `#{config['sshfs_path']} #{port} -o #{options} \
-      '#{mount['source']}' '#{destination}'`
+    puts "#{config['sshfs_path']} #{port} -o #{options} \ '#{mount['source']}' '#{destination}'"
+    `#{config['sshfs_path']} #{port} -o #{options} \ '#{mount['source']}' '#{destination}'`
   end
 end
