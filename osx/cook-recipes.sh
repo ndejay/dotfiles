e8d15b97a48a2f42464c8316e9cc03183798916c
Nicolas De Jay <ndj@pinkfilter.org>
2018-07-18 14:25:27 -0400

    Update recipes.

--- osx/cook-recipes.sh
+++ osx/cook-recipes.sh
@@ -5,46 +5,28 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # This script installs all the software I need on OS X so that I
 # don't have to. ;)
 
-brew tap homebrew/science
-
 RECIPES=(
-  Caskroom/cask/osxfuse
-  sshfs
-  encfs
+  Caskroom/cask/osxfuse sshfs encfs
 
   Caskroom/cask/google-chrome
-  Caskroom/cask/google-drive
+  Caskroom/cask/google-backup-and-sync
   Caskroom/cask/dropbox
   Caskroom/cask/evernote
   Caskroom/cask/mendeley-desktop
-  Caskroom/cask/crashplan
   Caskroom/cask/spectacle
-  Caskroom/cask/flux
-  Caskroom/cask/todoist
-# tomato
-# amphetamine
-  Caskroom/cask/bartender
+# Caskroom/cask/flux
+# Caskroom/cask/todoist
+# Caskroom/cask/bartender
   Caskroom/cask/alfred
 
-  Caskroom/cask/java
-  Caskroom/cask/xquartz
-  Caskroom/cask/iterm2
-  git
-  mosh
-  vim
-  htop
-  wget
-  curl
-  zsh
-  bash
-  the_silver_searcher
+  # adobe creative cloud
+  # microsoft office
+  # todoist
+  # tomato
 
-  findutils
-  gnu-indent
-  gnu-sed
-  gnutls
-  gnu-tar
-  gawk
+  Caskroom/cask/iterm2
+  git mosh vim htop wget curl zsh hhe_silver_searcher thefuck
+  coreutils findutils gnu-indent gnu-sed gnutls gnu-tar gawk
 )
 
 brew install ${RECIPES[@]}
