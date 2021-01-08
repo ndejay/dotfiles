c2650451feeabc24f73a5a9e65550bcfec967230
Nicolas De Jay <ndj@pinkfilter.org>
2021-01-08 11:38:35 -0500

    Update Homebrew recipes.

--- osx/cook-recipes.sh
+++ osx/cook-recipes.sh
@@ -6,27 +6,17 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # don't have to. ;)
 
 RECIPES=(
-  Caskroom/cask/osxfuse sshfs encfs
-
-  Caskroom/cask/google-chrome
-  Caskroom/cask/google-backup-and-sync
-  Caskroom/cask/dropbox
-  Caskroom/cask/evernote
-  Caskroom/cask/mendeley-desktop
-  Caskroom/cask/spectacle
-# Caskroom/cask/flux
-# Caskroom/cask/todoist
-# Caskroom/cask/bartender
-  Caskroom/cask/alfred
-
-  # adobe creative cloud
-  # microsoft office
-  # todoist
-  # tomato
-
-  Caskroom/cask/iterm2
-  git mosh vim htop wget curl zsh hhe_silver_searcher thefuck
+  git mosh vim htop wget curl zsh tmux
   coreutils findutils gnu-indent gnu-sed gnutls gnu-tar gawk
+  osxfuse sshfs encfs
+
+  iterm2 google-chrome spectacle flux bartender alfred appcleaner
+  google-backup-and-sync dropbox lastpass evernote
+  mendeley-desktop kindle spotify anki private-internet-access
+  # get 2Do online
+  pomodone
+  slack zoom microsoft-teams microsoft-office-2016
+  # https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Remote_Desktop_10.3.8_installer.pkg
 )
 
 brew install ${RECIPES[@]}
